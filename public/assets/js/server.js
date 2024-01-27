c3553a6562785f1a41b7472cab2b2e08d5fe56bb
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-01-26 19:00:55 -0500

    added code for server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -3,6 +3,27 @@ const path = require('path');
 // const bodyParsing = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = PORT || 3001;
 
 // for Middleware to parse JSON data use express.json() / express.urlencoded()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use('/api',api);
+
+app .use(express.static('public'));
+// home page / landing page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/assets/js/index.js'))
+);
+
+// the notes page
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/assets/js/index.js'))
+);
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
+
