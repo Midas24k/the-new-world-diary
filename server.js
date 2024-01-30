6f93fb66f91518d11bd714ceb3a261308610d3e2
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-01-29 19:30:44 -0500

    fixed alot of stuff

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const express = require('express');
 const path = require('path');
-const dbData = require(db/db.json)
+const dbData = require('./db/db.json');
+const fs = require('fs');
 // const bodyParsing = require('body-parser');
 
 const app = express();
-const PORT = PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 // for Middleware to parse JSON data use express.json() / express.urlencoded()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use('/api',api);
+//app.use('/api',api);
 
 app.use(express.static('public'));
 // home page / landing page
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/assets/js/index.js'))
-);
 
 // the notes page
 app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/assets/js/index.js'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
@@ -45,6 +43,7 @@ app.get('/api/notes', (req, res) => {
       }
     });
   });
+
 
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json 
@@ -74,6 +73,10 @@ app.post('/api/notes', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 
 app.listen(PORT, () =>
